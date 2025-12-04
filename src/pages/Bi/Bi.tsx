550fde34d48010449abc30873f27255d27d0c765
Const4ntino <adrianalvaradoconstantino@gmail.com>
2025-12-04 11:08:11 -0500

    Proyecto Iniciado

---
+++ src/pages/Bi/Bi.tsx
@@ -0,0 +1,5 @@
+export default function Bi() {
+  return (
+    <div>Bi</div>
+  )
+}
---
+++ src/routes.ts
@@ -0,0 +1,18 @@
+import App from './App.tsx'
+import { createBrowserRouter } from "react-router"
+import Landing from './pages/Landing/Landing.tsx'
+import Login from './pages/Login/Login.tsx'
+import Bi from './pages/Bi/Bi.tsx'
+
+export const router = createBrowserRouter([
+    // ruta índice (no necesita un path)
+    {
+        path: "/",
+        Component: App,
+        children: [
+            { index: true, Component: Landing },
+            { path: "login", Component: Login, },
+            { path: "bi", Component: Bi}
+        ]
+    },
+])
